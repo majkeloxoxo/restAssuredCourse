2339d1b43d070270382f66c38b4ddf334179eae5
Michał <michael.makowiecki@gmail.com>
2020-07-09 14:02:25 +0200

    kurs

--- src/test/java/pl/javastart/restassured/test/tasks/UserCreationTests.java
+++ src/test/java/pl/javastart/restassured/test/tasks/UserCreationTests.java
@@ -1,6 +1,7 @@
 package pl.javastart.restassured.test.tasks;
 
 import org.testng.annotations.Test;
+import pl.javastart.main.pojo.user.User;
 
 import static io.restassured.RestAssured.given;
 
@@ -9,25 +10,25 @@ public class UserCreationTests {
     @Test
     public void givenCorrectUserDataWhenCreateUserThenUserIsCreatedTest() {
 
-        String user = "{\n" +
-                "  \"id\": 445,\n" +
-                "  \"username\": \"firstuser\",\n" +
-                "  \"firstName\": \"Krzysztof\",\n" +
-                "  \"lastName\": \"Kowalski\",\n" +
-                "  \"email\": \"krzysztof@test.com\",\n" +
-                "  \"password\": \"password\",\n" +
-                "  \"phone\": \"+123456789\",\n" +
-                "  \"userStatus\": 1\n" +
-                "}";
+        User user = new User();
+        user.setId(445);
+        user.setUsername("firstuser");
+        user.setFirstName("Hideo");
+        user.setLastName("Kojima");
+        user.setEmail("videokojima@mgs.com");
+        user.setPassword("lalulilelo");
+        user.setPhone("+123456789");
+        user.setUserStatus(123);
 
         given().log().all()
-                .body(user).contentType("application/json")
+                .contentType("application/json")
+                .body(user)
                 .when().post("https://swaggerpetstore.przyklady.javastart.pl/v2/user")
                 .then().log().all().statusCode(200);
 
         given().log().all()
-                .pathParam("username", "firstuser")
                 .contentType("application/json")
+                .pathParam("username", user.getUsername())
                 .when().get("https://swaggerpetstore.przyklady.javastart.pl/v2/user/{username}")
                 .then().log().all().statusCode(200);
     }
